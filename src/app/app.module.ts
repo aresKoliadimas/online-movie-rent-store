725c3423be358e53d56516e3bb637643ffa8d178
Ares Koliadimas <ariskoliadimas@gmail.com>
2022-12-10 14:22:44 +0200

    creating movie card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MoviesListComponent } from './movies-list/movies-list.component';
+import { MovieCardComponent } from './movies-list/movie-card/movie-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MoviesListComponent
+    MoviesListComponent,
+    MovieCardComponent
   ],
   imports: [
     BrowserModule,
