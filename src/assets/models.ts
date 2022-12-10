fd05171da3d2fe1486189259292cbb76f110dd2a
Ares Koliadimas <ariskoliadimas@gmail.com>
2022-12-10 14:24:05 +0200

    creating movie model

---
+++ src/assets/models.ts
@@ -0,0 +1,10 @@
+export interface Movie {
+  uuid: string;
+  title: string;
+  pub_date: number;
+  duration: number;
+  rating: number;
+  description: string;
+  poster_url: string;
+  categories: string[];
+}
