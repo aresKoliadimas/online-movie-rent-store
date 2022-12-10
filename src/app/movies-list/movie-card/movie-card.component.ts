74d808442403c9edd360250ff1d4edd9855f106a
Ares Koliadimas <ariskoliadimas@gmail.com>
2022-12-10 19:40:01 +0200

    fixing movie card

--- src/app/movies-list/movie-card/movie-card.component.ts
+++ src/app/movies-list/movie-card/movie-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 import { Movie } from 'src/assets/models';
+import { MovieDetailsModalComponent } from './movie-details-modal/movie-details-modal.component';
 
 @Component({
   selector: 'app-movie-card',
@@ -9,7 +11,19 @@ import { Movie } from 'src/assets/models';
 export class MovieCardComponent implements OnInit {
   @Input() movie!: Movie;
 
-  ngOnInit(): void {
-    console.log(this.movie);
+  constructor(private modalCtrl: ModalController) {}
+
+  ngOnInit(): void {}
+
+  async openMovieModal(movie: Movie) {
+    const modal = await this.modalCtrl.create({
+      component: MovieDetailsModalComponent,
+      cssClass: 'movie-modal',
+      backdropDismiss: false,
+      componentProps: {
+        movie: movie,
+      },
+    });
+    return await modal.present();
   }
 }
