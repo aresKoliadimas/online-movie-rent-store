3bc86aef7efcb0138ad2801ffcae46d7eec87cf4
Ares Koliadimas <ariskoliadimas@gmail.com>
2022-12-10 14:29:45 +0200

    adding some functionality

--- src/app/movies-list/movies-list.component.ts
+++ src/app/movies-list/movies-list.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Movie } from 'src/assets/models';
+import moviesJson from '../../assets/movies.json';
 
 @Component({
   selector: 'app-movies-list',
   templateUrl: './movies-list.component.html',
-  styleUrls: ['./movies-list.component.scss']
+  styleUrls: ['./movies-list.component.scss'],
 })
 export class MoviesListComponent {
+  movies: Movie[] = [];
 
+  ngOnInit() {
+    this.movies = [...moviesJson.results];
+  }
 }
