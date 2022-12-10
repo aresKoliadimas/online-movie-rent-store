bd4da3b64bb0fa3c2a865e5e6c39e29bd34441a6
Ares Koliadimas <ariskoliadimas@gmail.com>
2022-12-10 19:40:31 +0200

    adding functionality to modal

--- src/app/movies-list/movie-card/movie-details-modal/movie-details-modal.component.ts
+++ src/app/movies-list/movie-card/movie-details-modal/movie-details-modal.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { Movie } from 'src/assets/models';
 
 @Component({
   selector: 'app-movie-details-modal',
   templateUrl: './movie-details-modal.component.html',
-  styleUrls: ['./movie-details-modal.component.scss']
+  styleUrls: ['./movie-details-modal.component.scss'],
 })
-export class MovieDetailsModalComponent {
+export class MovieDetailsModalComponent implements OnInit {
+  @Input() movie!: Movie;
+  hours!: number;
+  star = '⭐';
+  public stars = '';
 
+  constructor(private modalCtrl: ModalController) {}
+
+  ngOnInit(): void {
+    this.hours = Math.floor(this.movie.duration / 60);
+    this.stars = this.star.repeat(Math.floor(this.movie.rating));
+    console.log(this.movie);
+  }
+
+  onModalClose() {
+    this.modalCtrl.dismiss();
+  }
 }
