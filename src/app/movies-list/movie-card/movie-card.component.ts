db8d54b88fdd461b7fb6bc47b2ef62519f7c868c
Ares Koliadimas <ariskoliadimas@gmail.com>
2022-12-10 14:49:39 +0200

    adding some card functionality

--- src/app/movies-list/movie-card/movie-card.component.ts
+++ src/app/movies-list/movie-card/movie-card.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Movie } from 'src/assets/models';
 
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
-  styleUrls: ['./movie-card.component.scss']
+  styleUrls: ['./movie-card.component.scss'],
 })
 export class MovieCardComponent {
+  @Input() movie!: Movie;
 
+  ngOnInit() {
+    console.log(this.movie);
+  }
 }
