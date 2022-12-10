d0de44ee7fb6ec3c18ad7c454ea9a1016f42842b
Ares Koliadimas <ariskoliadimas@gmail.com>
2022-12-10 15:15:10 +0200

    adding movies list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MoviesListComponent } from './movies-list/movies-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: MoviesListComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
