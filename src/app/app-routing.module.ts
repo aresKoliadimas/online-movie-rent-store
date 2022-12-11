6bc115d9f7ad873301e33f70451044b280d5b049
Ares Koliadimas <ariskoliadimas@gmail.com>
2022-12-12 01:03:41 +0200

    adding AuthGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './shared/auth.guard';
 import { LoginComponent } from './login/login.component';
 import { MoviesListComponent } from './movies-list/movies-list.component';
 
 const routes: Routes = [
-  { path: '', component: MoviesListComponent },
+  { path: '', component: MoviesListComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
 ];
 
