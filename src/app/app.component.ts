cc579b16b2a5a322450ff6f745286a8863c39f3d
Ares Koliadimas <ariskoliadimas@gmail.com>
2022-12-13 13:10:40 +0200

    adding todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  // TODO: add spinners
   title = 'online-movie-rent-store';
 }
